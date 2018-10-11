b78859d70f32ed588b722eba44ccf37f78c22975
Manuel Cabarcos Baulina <crac.master@hotmail.com>
2018-10-11 16:30:56 +0200

    version stable

--- src/myDTWtest.java
+++ src/myDTWtest.java
@@ -1,4 +1,7 @@
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,100 +12,38 @@ import fr.enseeiht.danck.voice_analyzer.MFCC;
 import fr.enseeiht.danck.voice_analyzer.WindowMaker;
 import fr.enseeiht.danck.voice_analyzer.defaults.DTWHelperDefault;
 
-/*public class myDTWtest {
-
-		protected static final int MFCCLength = 13;
-
-		public static void main(String[] args) throws IOException, InterruptedException {
-			
-			//DTWHelper myDTWHelper= new myDTW();
-			DTWHelper DTWHelperDefault= new DTWHelperDefault();
-			 
-			// Chemin de recherche des fichiers sons
-		    String base = "/test_res/audio/";
-		    
-		    // Appel a l'extracteur par defaut (calcul des MFCC)
-		    Extractor extractor = Extractor.getExtractor();
-		    
-			// Etape 1. Lecture de Alpha
-		    List<String> files = new ArrayList<>();
-		    files.add(base + "Alpha.csv");
-		    WindowMaker windowMaker = new MultipleFileWindowMaker(files);
-		    
-		    // Etape 2. Recuperation des MFCC du mot Alpha
-		    MFCC[] mfccsAlpha = new MFCC[MFCCLength];
-	        for (int i = 0; i < mfccsAlpha.length; i++) {
-	            mfccsAlpha[i] = extractor.nextMFCC(windowMaker);
-	        }
-	        
-	        // Etape 3. Construction du Field (ensemble de MFCC) de alpha
-	        Field alphaField= new Field(mfccsAlpha);
-	        
-	        // Si on veut rajouter de nouveaux mots, il suffit de repeter les etapes 1 a 3
-	        
-	        // Par ex., on peut tester que la distance entre alpha et alpha c'est 0
-	       // float mydistanceAlphaAlpha= myDTWHelper.DTWDistance(alphaField, alphaField);
-	        float distanceAlphaAlphadefault= DTWHelperDefault.DTWDistance(alphaField, alphaField);
-	        
-	       // System.out.println("myDTW - valeur distance Alpha-Alpha calculee : "+mydistanceAlphaAlpha);
-	        System.out.println("DTWHelperDefault - valeur distance Alpha-Alpha calculee : "+distanceAlphaAlphadefault);
-		
-	        // Calcul de la distance entre Alpha et Bravo
-	        
-	        // Etape 1. Lecture de Bravo
-	        files= new ArrayList<>();
-		    files.add(base + "Bravo.csv");
-		    windowMaker = new MultipleFileWindowMaker(files);
-		    
-		 // Etape 2. Recuperation des MFCC du mot Bravo
-		    MFCC[] mfccsBravo= new MFCC[MFCCLength];
-	        for (int i = 0; i < mfccsBravo.length; i++) {
-	            mfccsBravo[i] = extractor.nextMFCC(windowMaker);
-	       
-	        }
-	        
-	        // Etape 3. Construction du Field (ensemble de MFCC) de Bravo
-	        Field bravoField= new Field(mfccsBravo);
-	        
-	       //float mydistanceAlphaBravo= myDTWHelper.DTWDistance(alphaField, bravoField);
-	        float distanceAlphaBravodefault= DTWHelperDefault.DTWDistance(alphaField, bravoField);
-	        
-	        //System.out.println("myDTW - valeur distance Alpha-Bravo calculee : "+mydistanceAlphaBravo);
-	        System.out.println("DTWHelperDefault - valeur distance Alpha-Bravo calculee : "+distanceAlphaBravodefault);
-		}
-
-}*/
 
 public class myDTWtest {
-
-	//protected static final int MFCCLength = 13;
 	
 			// Fonction permettant de calculer la taille des Fields 
-			//c'est-à-dire le nombre de MFCC du Field 
-				static int FieldLength(String fileName) throws IOException {
-				int counter= 0;
-				File file = new File(System.getProperty("user.dir") + fileName);
-	            for (String line : Files.readAllLines(file.toPath(), Charset.defaultCharset())) {
-	            	counter++;
-	            }
-	            return 2*Math.floorDiv(counter, 512);
-			}
+			//c'est-ï¿½-dire le nombre de MFCC du Field 
+	static int FieldLength(String fileName) throws IOException {
+		int counter= 0;
+		File file = new File(System.getProperty("user.dir") + fileName);
+	    for (String line : Files.readAllLines(file.toPath(), Charset.defaultCharset())) {
+	           	counter++;
+	    }
+	    return 2*Math.floorDiv(counter, 512);
+	}
 	
 	public static Field[] loadingAudioFiles (String nomLocuteur, String[] vocabulaire) throws IOException, InterruptedException
 	{
 		Extractor extractor = Extractor.getExtractor();
 	    String base = "/test_res/audio/csv/" + nomLocuteur + "_";
 	    Field[] fieldVocabulaire = new Field[vocabulaire.length];
-	    MFCC[][] mfccVocabulaire = new MFCC[vocabulaire.length][FieldLength];		    
+	    MFCC[][] mfccVocabulaire = new MFCC[vocabulaire.length][];
 	    
+	    String nomF;
+	    int MFCCLength; 
 	    for(int i = 0; i < vocabulaire.length; i++)
-	    {
+	    {	nomF=base + vocabulaire[i] + ".csv";
 	    	// Etape 1. Lecture du fichier
 		    List<String> files = new ArrayList<>();
-		    files.add(base + vocabulaire[i] + ".csv");
+		    files.add(nomF);
 		    WindowMaker windowMaker = new MultipleFileWindowMaker(files);
 		    
 		    // Etape 2. Recuperation des MFCC du mot
+		     MFCCLength =FieldLength(nomF);
 		    mfccVocabulaire[i] = new MFCC[MFCCLength];
 	        for (int j = 0; j < mfccVocabulaire[i].length; j++) {
 	        	mfccVocabulaire[i][j] = extractor.nextMFCC(windowMaker);
@@ -129,7 +70,7 @@ public class myDTWtest {
 	    String[] vocabulaire = {"arretetoi","atterrissage","avance","decollage","droite","etatdurgence",
 	    						"faisunflip","gauche","plusbas","plushaut","recule","tournedroite","tournegauche"};
 	    
-	    //Chargement du locuteur référent
+	    //Chargement du locuteur rï¿½fï¿½rent
 	    Field[] motLocuteur = loadingAudioFiles(locuteur,vocabulaire);
 	    
         
@@ -164,7 +105,7 @@ public class myDTWtest {
 		    	int minIdx = 0;
 		    	float min = matRes[motHypIdx][0];
 
-		    	//Récupération de l'indice de la valeur min
+		    	//Rï¿½cupï¿½ration de l'indice de la valeur min
 		    	for (int ind = 0; ind < vocabulaire.length; ind++)
 		    	{
 		    		if (matRes[motHypIdx][ind] < min)
@@ -184,7 +125,7 @@ public class myDTWtest {
 	    }
 	    tauxReco /= (vocabulaire.length * hypoteses.length);
 	    
-	    //Affichage des résultats
+	    //Affichage des rï¿½sultats
 	    for (int i = 0; i < vocabulaire.length; i++)
 	    {
 	    	for (int j = 0; j < vocabulaire.length; j++)
